93f31db42a8110175beb9dad7596c5b1b7aaa316
Rei Ryuki <46316533+reiryuki@users.noreply.github.com>
2022-11-12 14:53:36 +0700

    Enhancements

--- post-fs-data.sh
+++ post-fs-data.sh
@@ -8,7 +8,7 @@ set -x
 # run
 FILE=$MODPATH/sepolicy.sh
 if [ -f $FILE ]; then
-  sh $FILE
+  . $FILE
 fi
 
 # context
@@ -22,7 +22,7 @@ rm -f /data/adb/modules/*/system/app/MotoSignatureApp/.replace
 # cleaning
 FILE=$MODPATH/cleaner.sh
 if [ -f $FILE ]; then
-  sh $FILE
+  . $FILE
   rm -f $FILE
 fi
 
--- service.sh
+++ service.sh
@@ -13,8 +13,16 @@ PKG=com.motorola.timeweatherwidget
 pm grant $PKG android.permission.ACCESS_FINE_LOCATION
 pm grant $PKG android.permission.ACCESS_COARSE_LOCATION
 pm grant $PKG android.permission.ACCESS_BACKGROUND_LOCATION
+pm grant $PKG android.permission.READ_EXTERNAL_STORAGE
 if [ "$API" -ge 30 ]; then
   appops set $PKG AUTO_REVOKE_PERMISSIONS_IF_UNUSED ignore
+fi
+if [ "$API" -ge 33 ]; then
+  pm grant $PKG android.permission.READ_MEDIA_IMAGES
+  pm grant $PKG android.permission.READ_MEDIA_AUDIO
+  pm grant $PKG android.permission.READ_MEDIA_VIDEO
+  pm grant $PKG android.permission.POST_NOTIFICATIONS
+  appops set $PKG ACCESS_RESTRICTED_SETTINGS allow
 fi
 
 # grant
@@ -25,6 +33,13 @@ pm grant $PKG android.permission.READ_PHONE_STATE
 pm grant $PKG android.permission.CALL_PHONE
 if [ "$API" -ge 30 ]; then
   appops set $PKG AUTO_REVOKE_PERMISSIONS_IF_UNUSED ignore
+fi
+if [ "$API" -ge 33 ]; then
+  pm grant $PKG android.permission.READ_MEDIA_IMAGES
+  pm grant $PKG android.permission.READ_MEDIA_AUDIO
+  pm grant $PKG android.permission.READ_MEDIA_VIDEO
+  pm grant $PKG android.permission.POST_NOTIFICATIONS
+  appops set $PKG ACCESS_RESTRICTED_SETTINGS allow
 fi
 
 
